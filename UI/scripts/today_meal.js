9b3a519b9e3b1eafa27f54bbdb3c4014b7c97c3a
Odumah Solomon <odumahs@gmail.com>
2018-04-19 08:27:24 +0100

    Add script to handle adding meals to menu

---
+++ UI/scripts/today_meal.js
@@ -0,0 +1,37 @@
+function myFunction() {
+	/*let all_meals = ['Rice & stew', 'Eba & Efo', 'Fufu & Egusi', 'Pancake', 'Friedrice', 'Bread & Egg'];*/
+	let today_meal = [];
+	let selectedMeal = '';
+
+	let meal = document.forms[0];
+	let i;
+	for (i = 0; i < meal.length; i++) {
+		if (meal[i].checked) {
+			selectedMeal = meal[i].value;
+			today_meal.push(selectedMeal);
+		}
+	}
+
+	let ulElement = document.getElementById('mealList');
+	ulElement.innerHTML = '';
+	let btn = document.getElementById('save_menu_button');
+
+	if (today_meal.length > 0) {
+		btn.removeAttribute('disabled');
+	}else{
+		btn.setAttribute('disabled','disabled');
+	}
+	
+	console.log(today_meal)
+
+	for (let j = 0; j < today_meal.length; j++) {	
+		/*let listText = document.createTextNode(today_meal[j]);
+		
+		let myElement = document.createElement('li');
+		ulElement.appendChild(myElement);	
+		myElement.appendChild(listText);*/
+		ulElement.innerHTML += `<li>${today_meal[j]}</li>`;
+	}	
+
+   
+}
