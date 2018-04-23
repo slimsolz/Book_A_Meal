9c3d6a60ed72426e7cabe7ea11dc70fe10766a97
Odumah Solomon <odumahs@gmail.com>
2018-04-23 21:11:28 +0100

    install eslint and refactor code

--- server/app.js
+++ server/app.js
@@ -1,26 +1,25 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const index = require('./routes/index');
 
 
 const app = express();
+
+// port
 const port = process.env.PORT || 3000;
 
-//log request to console
+// log request to console
 app.use(logger('dev'));
 
-//parse incoming request data
+// parse incoming request data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) =>{
-	res.json({
-		message: 'Welcome To Book-A-Meal App'
-	});
-})
+app.use('/api/v1/', index);
 
-app.listen(port, () =>{
-	console.log(`App started on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`App started on port ${port}`);
+});
 
 module.exports = app;
---
+++ server/routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) =>{
+	res.json({
+		message: 'Welcome to Book-A-Meal App'
+	});
+});
+
+module.exports = router;
