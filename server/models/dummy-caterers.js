f0c34a9b665818eed2ebc839138543ac46d0e578
Odumah Solomon <odumahs@gmail.com>
2018-04-24 10:45:41 +0100

    Add dummy caterers

---
+++ server/models/dummy-caterers.js
@@ -0,0 +1,17 @@
+const db = [];
+
+db.caterers = [{
+  id: 1,
+  email: 'odumahs@gmail.com',
+  username: 'slimsolz',
+  password: 'slimsolz'
+},
+{
+  id: 2,
+  email: 'solo@ymail.com',
+  username: 'solz',
+  password: 'kobebryant'
+},
+];
+
+export default db;
