9ca6966088ddd4ac3e97250c6a501df18f071c5f
Odumah Solomon <odumahs@gmail.com>
2018-04-25 22:31:10 +0100

    Add customer's signup and signin routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,19 +1,24 @@
 import express from 'express';
-import db from '../models/dummy-caterers';
+import db from '../models/dummy-db';
 import CatererController from '../controllers/catererController';
-
+import CustomerController from '../controllers/customerController';
 
 
 const router = express.Router();
 
-router.get('/', (req, res, next) =>{
-	res.json({
-		message: 'Welcome to Book-A-Meal App',
-		caterers: db.caterers
-	});
+router.get('/', (req, res, next) => {
+  res.json({
+    message: 'Welcome to Book-A-Meal App',
+    caterers: db.caterers
+  });
 });
 
+//Caterers
 router.post('/caterer/signup', CatererController.signUp);
 router.post('/caterer/signin', CatererController.signIn);
+
+//Users
+router.post('/customer/signup', CustomerController.signUp);
+router.post('/customer/signin', CustomerController.signIn);
 
 export default router;
