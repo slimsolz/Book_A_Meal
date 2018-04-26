ed9d7aa9421e0bcd982802997397d0b25c2a8d71
Odumah Solomon <odumahs@gmail.com>
2018-04-26 18:22:36 +0100

    Refactor tests into different file

--- server/tests/customer.test.js
+++ server/tests/customer.test.js
@@ -5,20 +5,22 @@ import app from '../app';
 const { expect } = chai;
 chai.use(chaiHttp);
 
-const Caterer = {
-  id: 3,
-  email: 'user-test@gmail.com',
-  username: 'admin',
-  password: 'passworD'
+
+const Customer = {
+	id: 4,
+	email: 'user4@gmail.com',
+	username: 'user4',
+	password: 'pass4'
 };
 
+/*CUSTOMER*/
 
 // POST signup
-describe('POST caterer/signup', () => {
-  it('should create new caterer', (done) => {
+describe('POST customer/signup', () => {
+  it('should create new customer', (done) => {
     chai.request(app)
-      .post('/api/v1/caterer/signup')
-      .send(Caterer)
+      .post('/api/v1/customer/signup')
+      .send(Customer)
       .end((err, res) => {
         expect(res).to.have.status(201);
         expect(res.body).to.be.a('object');
@@ -29,7 +31,7 @@ describe('POST caterer/signup', () => {
   // POST sign up - should return 400 if no email
   it('should return 400 if no email', (done) => {
   	chai.request(app)
-  		.post('/api/v1/caterer/signup')
+  		.post('/api/v1/customer/signup')
   		.send({
   			id: 1,
   			email: '',
@@ -46,7 +48,7 @@ describe('POST caterer/signup', () => {
   // POST sign up - should return 400 if no username
   it('should return 400 if no username', (done) => {
   	chai.request(app)
-  		.post('/api/v1/caterer/signup')
+  		.post('/api/v1/customer/signup')
   		.send({
   			id: 1,
   			email: 'test@gmail.com',
@@ -63,7 +65,7 @@ describe('POST caterer/signup', () => {
   // POST sign up - should return 400 if no password
   it('should return 400 if no password', (done) => {
   	chai.request(app)
-  		.post('/api/v1/caterer/signup')
+  		.post('/api/v1/customer/signup')
   		.send({
   			id: 1,
   			email: 'test@gmail.com',
@@ -78,11 +80,11 @@ describe('POST caterer/signup', () => {
   });
 });
 
-describe("'Bad Request' POST caterer/signin", () => {
+describe("'Bad Request' POST customer/signin", () => {
   // POST sign in - should return 400 if no username
   it('should return 400 if no username', (done) => {
   	chai.request(app)
-  		.post('/api/v1/caterer/signin')
+  		.post('/api/v1/customer/signin')
   		.send({
   			username: '',
   			password: 'slim'
@@ -97,7 +99,7 @@ describe("'Bad Request' POST caterer/signin", () => {
   // POST sign in - should return 400 if no password
   it('should return 400 if no password', (done) => {
   	chai.request(app)
-  		.post('/api/v1/caterer/signin')
+  		.post('/api/v1/customer/signin')
   		.send({
   			username: 'solz',
   			password: ''
@@ -112,7 +114,7 @@ describe("'Bad Request' POST caterer/signin", () => {
   // POST sign in - should return 400 if undefined
   it('should return 400 if no undefined', (done) => {
   	chai.request(app)
-  		.post('/api/v1/caterer/signin')
+  		.post('/api/v1/customer/signin')
   		.send({
   			username: undefined,
   			password: undefined
@@ -127,7 +129,7 @@ describe("'Bad Request' POST caterer/signin", () => {
   // POST sign in - should return 400 if username and password is wrong
 /*  it('should return 400 if username and password is wrong', (done) => {
   	chai.request(app)
-  		.post('/api/v1/caterer/signin')
+  		.post('/api/v1/customer/signin')
   		.send({
   			username: 'jey@gmail',
   			password: 'bingo'
@@ -140,22 +142,21 @@ describe("'Bad Request' POST caterer/signin", () => {
   });*/
 });
 
-describe('POSt caterer/signin', () => {
+describe('POST customer/signin', () => {
   // POST sign in - should return 200 for successful login
   it('should return 200 for successful login', (done) => {
   	chai.request(app)
-  		.post('/api/v1/caterer/signin')
+  		.post('/api/v1/customer/signin')
   		.send({
-  			username: 'admin',
-  			password: 'passworD'
+  			username: Customer.username,
+  			password: Customer.password
   		})
   		.end((err, res) => {
   			expect(res).to.have.status(200);
   			expect(res.body).to.be.a('object');
+  			expect(res.body).to.have.property('message');
+  			expect(res.body.message).to.be.equal('Login successful');
   			done();
   		});
   });
 });
-
-
-
