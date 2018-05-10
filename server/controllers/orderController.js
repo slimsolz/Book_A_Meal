9e3b7792720de9d0cb7adab33e6f35297e08998c
Odumah Solomon <odumahs@gmail.com>
2018-05-10 04:01:32 +0100

    Add PUT /orders/:id route

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -25,4 +25,56 @@ export default class OrderController{
 		});		
 	}
 
+	//Modify order
+	static modifyOrder(req, res){
+		const { quantity, total, deliveryAddress, status } = req.body;
+		const { userId } = req;
+		const mealId = 4;
+
+
+		Order.findById(req.params.id)
+			.then((order) => {
+				console.log(order.status)
+				if (!order) {
+					return res.status(404).json({
+						status: 'error',
+						message: 'Order not found'
+					});
+				}
+				if (order.status === 'Approved' || order.status === 'In Process' ) {
+					return res.status(404).json({
+						status: 'error',
+						message: 'Order cannot be modified'
+					});
+				}
+				order.update({
+					quantity: quantity || order.quantity,
+					total: total || order.total,
+					deliveryAddress: deliveryAddress || order.deliveryAddress,
+					status: status || order.status,
+					userId,
+					mealId
+				}).then((updatedOrder) => {
+					if (updatedOrder) {
+						return res.status(200).json({
+							status: 'success',
+							message: 'Order updated Successfully',
+							updatedOrder: {
+								quantity: updatedOrder.quantity,
+								total: updatedOrder.total,
+								address: updatedOrder.deliveryAddress,
+								status: updatedOrder.status
+							}
+						});
+					}
+				});
+			}).catch((err) => {
+				return res.status(500).json({
+					status: 'error',
+					message: 'Server Error'
+				});
+			});
+	}
+
+
 }
